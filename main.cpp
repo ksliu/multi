2a084b2c6398695a6efe9012e21edf5d1e2957a2
ksliu <ksliu10@gmail.com>
2011-09-27 19:00:31 +0800

    test main

--- main.cpp
+++ main.cpp
@@ -1,42 +1,64 @@
-#include <iostream>
-#include <fstream>
 #include <string>
-#include <vector>
 #include <cstdlib>
 #include "protein.h"
-
-#include "multiString.h"
-#include "pairString.h"
-
 #include "multiAlign.h"
 
 using namespace std;
 
-int main()
+void test();
+bool RUN_TEST = true;
+
+int main(int argc, char **argv)
+{
+    if (RUN_TEST)
+    {
+        test();
+        return 0;
+    }
+
+    if (argc < 6)
+    {
+        cout << "program -o output_file pdb_file1 pdb_file2 pdb_file3..." << endl;
+        exit(1);
+    }
+
+    string outputFile = argv[2];
+    int numPDB = argc - 3;
+
+    if (numPDB < 3)
+    {
+        cout << "input at least 3 PDB files" << endl;
+        exit(1);
+    }
+
+    protein * pro = new protein [numPDB];
+    for (int i=0; i <numPDB; i++)
+        pro[i].loadFile(argv[i+3]);
+
+
+    MultiAlign ma (pro, numPDB);
+    ma.run();
+    ma.outputAlignResult(outputFile);
+
+    delete [] pro;
+
+    return 0;
+}
+
+void test()
 {
     const int N = 3;
-//    string pdbFiles[N] = {"1BAB.pdb", "1HLB.pdb", "1HLM.pdb"};
-    string pdbFiles[N] = { "1HLB.pdb", "1HLM.pdb", "1BAB.pdb"};
-//    string xyzFiles[N] = {"1BAB.xyz", "1HLB.xyz", "1HLM.xyz"};
+    //    string pdbFiles[N] = {"1BAB.pdb", "1HLB.pdb", "1HLM.pdb"};
+    string pdbFiles[N] = {"1HLB.pdb", "1HLM.pdb", "1BAB.pdb"};
 
     protein pro[N];
-    vector<string>pcl;
-
     for (int i=0; i<N; i++)
     {
         pro[i].loadFile(pdbFiles[i]);
         //        pro[i].toXYZFile(xyzFiles[i]);
-        pcl.push_back(pro[i].cl);
     }
 
     MultiAlign ma(pro, N);
-
-    ma.outputAlignResult("multiAlign1.txt");
-
     ma.run();
-
-    ma.outputAlignResult("multiAlign2.txt");
-
-
-    return 0;
+    ma.outputAlignResult("multiAlign.txt");
 }
